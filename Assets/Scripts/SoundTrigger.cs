02ed61ed96b9b986295081c567b482daeb971f87
murapong <murapong@gmail.com>
2013-09-08 00:06:26 +0900

    サウンド再生処理を追加

---
+++ Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTrigger : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        uint bankID;
+        AkSoundEngine.LoadBank("Car.bnk", AkSoundEngine.AK_DEFAULT_POOL_ID, out bankID);
+        AkSoundEngine.PostEvent(AK.EVENTS.PLAY_ENGINE, gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+}
